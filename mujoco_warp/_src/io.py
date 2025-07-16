2855fe1a27527b762c50969f64103a1cb7c47085
Taylor Howell <taylorhowell@google.com>
2025-07-15 23:53:30 -0700

    io.put_model check for flex collisions (#502)
    * io.put_model check for flex collisions
    
    * update put_model
    
    * update flex/cloth.xml and flex/floppy.xml

--- mujoco_warp/_src/io.py
+++ mujoco_warp/_src/io.py
@@ -97,6 +97,9 @@ def put_model(mjm: mujoco.MjModel) -> types.Model:
   if mjm.nflex > 1:
     raise NotImplementedError("Only one flex is unsupported.")
 
+  if ((mjm.flex_contype != 0) | (mjm.flex_conaffinity != 0)).any():
+    raise NotImplementedError("Flex collisions are not implemented.")
+
   if mjm.geom_fluid.any():
     raise NotImplementedError("Ellipsoid fluid model not implemented.")
 
