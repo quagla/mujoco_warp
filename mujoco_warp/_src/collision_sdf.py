22a14172d6bf7d3a4d68f3667f2c6d88264911fa
Alessio Quaglino <quaglino@google.com>
2025-08-01 09:15:18 +0100

    remove trailing space

--- mujoco_warp/_src/collision_sdf.py
+++ mujoco_warp/_src/collision_sdf.py
@@ -501,7 +501,7 @@ def _sdf_narrowphase(
   aabb_size = geom_aabb[g1, 1]
   identity = wp.mat33(
     1.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 
+    0.0, 1.0, 0.0,
     0.0, 0.0, 1.0
 )
   aabb1 = transform_aabb(aabb_pos, aabb_size, wp.vec3(0.0), identity)
