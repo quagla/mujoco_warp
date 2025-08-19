002e8beed956d6104947fc3f4664f357585c2f13
Julia von Muralt <jvonmuralt@nvidia.com>
2025-08-20 00:23:55 +0200

    fix

--- mujoco_warp/_src/collision_driver_test.py
+++ mujoco_warp/_src/collision_driver_test.py
@@ -35,7 +35,7 @@ def sample_sdf_kernel(
   # In:
   points: wp.array(dtype=wp.vec3),
   volume_data: VolumeData,
-  # Data out:
+  # Out:
   results_out: wp.array(dtype=float),
 ):
   """Kernel to sample SDF values at given points using Warp volume."""
