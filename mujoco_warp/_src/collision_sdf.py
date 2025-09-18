f8a46cc52f3d21cf441c39c5b624f0094f9c2d02
Alessio Quaglino <102665781+quagla@users.noreply.github.com>
2025-09-18 10:29:54 +0100

    Change children order in the SDF octree (#712)
    * remove wp.Volume
    
    * auto-format
    
    * fix sensor
    
    * fix kernel arguments
    
    * fix small bugs
    
    * fix indentation
    
    * add box_project function
    
    * use analytic gradient where available
    
    * remove redundant aabb check
    
    * fix test requirement
    
    * remove outdated test
    
    * update octree ordering after mujoco change
    
    * add check to skip octree nodes

--- mujoco_warp/_src/collision_sdf.py
+++ mujoco_warp/_src/collision_sdf.py
@@ -232,6 +232,7 @@ def find_oct(
   rx = vec8f(0.0)
   ry = vec8f(0.0)
   rz = vec8f(0.0)
+  eps = 1e-6
 
   while niter > 0:
     niter -= 1
@@ -243,6 +244,17 @@ def find_oct(
 
     vmin = oct_aabb[node, 0] - oct_aabb[node, 1]
     vmax = oct_aabb[node, 0] + oct_aabb[node, 1]
+
+    if (
+      p[0] + eps < vmin[0]
+      or p[0] - eps > vmax[0]
+      or p[1] + eps < vmin[1]
+      or p[1] - eps > vmax[1]
+      or p[2] + eps < vmin[2]
+      or p[2] - eps > vmax[2]
+    ):
+      continue
+
     coord = wp.cw_div(p - vmin, vmax - vmin)
 
     # check if the node is a leaf
@@ -270,9 +282,9 @@ def find_oct(
       return node, (rx, ry, rz)
 
     # compute which of 8 children to visit next
-    x = 1 if coord[0] < 0.5 else 0
-    y = 1 if coord[1] < 0.5 else 0
-    z = 1 if coord[2] < 0.5 else 0
+    x = 0 if coord[0] < 0.5 else 1
+    y = 0 if coord[1] < 0.5 else 1
+    z = 0 if coord[2] < 0.5 else 1
     stack = oct_child[node][4 * z + 2 * y + x]
 
   wp.print("ERROR: Node not found\n")
