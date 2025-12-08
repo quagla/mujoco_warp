a22a683ec82b6b03267d58c584aeb26b2931eada
Taylor Howell <taylorhowell@google.com>
2025-12-08 10:35:27 -0800

    update primitive narrowphase (#889)
    * update primitive narrowphase
    
    * update conftest to generate all primitive collision kernels

--- conftest.py
+++ conftest.py
@@ -15,6 +15,10 @@
 
 import warp as wp
 
+import mujoco_warp as mjw
+from mujoco_warp import GeomType
+from mujoco_warp import test_data
+
 collect_ignore = ["benchmark/mujoco_menagerie"]
 
 
@@ -36,3 +40,66 @@ def pytest_configure(config):
     wp.config.verify_cuda = True
   if config.getoption("--lineinfo"):
     wp.config.lineinfo = True
+
+  ## initialize primitive colliders
+  # clear cache
+  mjw._src.collision_primitive._PRIMITIVE_COLLISION_TYPES.clear()
+  mjw._src.collision_primitive._PRIMITIVE_COLLISION_FUNC.clear()
+
+  # map enum to string
+  geom_str = {
+    GeomType.PLANE: "plane",
+    GeomType.SPHERE: "sphere",
+    GeomType.CAPSULE: "capsule",
+    GeomType.ELLIPSOID: "ellipsoid",
+    GeomType.CYLINDER: "cylinder",
+    GeomType.BOX: "box",
+    GeomType.MESH: "mesh",
+  }
+
+  # generate kernel
+  for geomtype in mjw._src.collision_primitive._PRIMITIVE_COLLISIONS.keys():
+    if geomtype[0] == GeomType.PLANE:
+      obj0 = '<geom type="plane" size="10 10 .01"/>'
+    else:
+      obj0 = f"""
+      <body>
+        <geom type="{geom_str[geomtype[0]]}" size=".1 .1 .1"/>
+        <joint/>
+      </body>
+    """
+
+    if geomtype[1] == GeomType.MESH:
+      obj1 = f"""
+        <body>
+          <geom type="{geom_str[geomtype[1]]}" mesh="mesh" size=".1 .1 .1"/>
+          <joint/>
+        </body>
+      """
+      mesh = """
+      <asset>
+        <mesh name="mesh" builtin="sphere" params="0"/>
+      </asset>
+      """
+    else:
+      obj1 = f"""
+        <body>
+          <geom type="{geom_str[geomtype[1]]}" size=".1 .1 .1"/>
+          <joint/>
+        </body>
+      """
+      mesh = ""
+
+    _, _, m, d = test_data.fixture(
+      xml=f"""
+    <mujoco>
+      {mesh}
+      <worldbody>
+       {obj0}
+       {obj1}
+      </worldbody>
+    </mujoco>
+    """
+    )
+
+    mjw.primitive_narrowphase(m, d)
