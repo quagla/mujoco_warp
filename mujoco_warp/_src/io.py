5a86ec28aa07741eb2e000d158f4ca4068ec146e
Mustafa H <34825877+StafaH@users.noreply.github.com>
2026-03-03 18:02:07 -0500

    Fix render_rgb and render_depth asserts (#1201)

--- mujoco_warp/_src/io.py
+++ mujoco_warp/_src/io.py
@@ -2537,18 +2537,18 @@ def create_render_context(
 
   cam_res_arr = wp.array(active_cam_res, dtype=wp.vec2i)
 
-  if render_rgb and isinstance(render_rgb, bool):
-    render_rgb = [render_rgb] * ncam
-  elif render_rgb is None:
+  if render_rgb is None:
     render_rgb = [mjm.cam_output[i] & mujoco.mjtCamOutBit.mjCAMOUT_RGB for i in active_cam_indices]
+  elif isinstance(render_rgb, bool):
+    render_rgb = [render_rgb] * ncam
 
-  if render_depth and isinstance(render_depth, bool):
-    render_depth = [render_depth] * ncam
-  elif render_depth is None:
+  if render_depth is None:
     render_depth = [mjm.cam_output[i] & mujoco.mjtCamOutBit.mjCAMOUT_DEPTH for i in active_cam_indices]
+  if isinstance(render_depth, bool):
+    render_depth = [render_depth] * ncam
 
   assert len(render_rgb) == ncam and len(render_depth) == ncam, (
-    f"Render RGB and depth must be provided for all active cameras (got {len(render_rgb)}, {len(render_depth)}, expected {ncam})"
+    f"render_rgb and render_depth must be a bool or a list of bools with length {ncam}"
   )
 
   rgb_adr = -1 * np.ones(ncam, dtype=int)
